7ad5995859f398e79f108cace1bee69daa37ecd4
maaazahmed <maazahmed2k16@gmail.com>
2022-11-10 14:06:10 -0800

    updated

--- ShoppingStore.sol
+++ ShoppingStore.sol
@@ -19,9 +19,16 @@ contract DCStore {
         mapping(uint=>Products) products;
     }
 
+    struct Carts {
+        address storeAddress;
+        uint productId;
+        uint quantity;
+        uint total;
+    }
+
     mapping(address=>StoresList) public storesList; 
     
-    mapping(address=>mapping(uint=>uint)) public cartList;
+    mapping(address=>mapping(uint=>Carts)) public cartList;
     
 
     constructor(){
@@ -60,10 +67,30 @@ contract DCStore {
 
 
    function addToCart(address buer_address, uint _productId, address _storeAddress ,uint _quantity ) public {
-       StoresList storage fineStore =  storesList[_storeAddress]; 
-       require(fineStore.owner  == _storeAddress , "Ivalid store!");
-       require(fineStore.products[_productId].quantity >= _quantity, "Not availble");
-       cartList[buer_address][_productId] = _quantity;
+        StoresList storage fineStore =  storesList[_storeAddress]; 
+        require(fineStore.owner  == _storeAddress , "Ivalid store!");
+        require(fineStore.products[_productId].quantity >= _quantity, "Out of stock");
+        Carts storage cart = cartList[buer_address][_productId];
+        cart.storeAddress = _storeAddress;
+       cart.productId = _productId;
+       cart.quantity = _quantity;
+      cart.total += cartList[buer_address][_productId-1].total;
    }
-  
+
+
+    function purchaseNow(uint[] memory cartItems) public payable {
+         uint totalAmount = cartList[msg.sender][cartItems[cartItems.length-1]].total; 
+         uint reamining = totalAmount;
+         require(msg.value >= totalAmount, "You did not pay enough amount of ether" );
+        for(uint index = 0; index < cartItems.length; index++){
+             Carts storage crt = cartList[msg.sender][cartItems[index]];
+             Products storage prd = storesList[crt.storeAddress].products[cartItems[index]]; 
+             require(crt.quantity >= prd.quantity, "Out of stock.");
+             address payable sendEth  = payable(crt.storeAddress);
+             uint payableAmout = (reamining - prd.price) - reamining;
+             reamining = reamining - prd.price;
+        }    
+
+
+    }
 }
